84b30b9ad45154f5a4141244ed2b358bcbce43d2
DanielSchwartzCS <98420686+DanielSchwartzCS@users.noreply.github.com>
2025-03-28 13:07:03 -0700

    left/right square bracket token

--- src/main/java/com/classhole/compiler/lexer/delimiters/RightSquareBracketToken.java
+++ src/main/java/com/classhole/compiler/lexer/delimiters/RightSquareBracketToken.java
@@ -3,6 +3,12 @@ package com.classhole.compiler.lexer.delimiters;
 import com.classhole.compiler.lexer.Token;
 
 public record RightSquareBracketToken(int line, int column) implements Token {
+<<<<<<< HEAD
   @Override
   public String getLexeme() { return "]"; }
 }
+=======
+    @Override
+    public String getLexeme() { return "]"; }
+}
+>>>>>>> 8fa9016 (left/right square bracket token)
