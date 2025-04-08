dadbf75885e9eebf214834b8a7b7a5eba2f5ca12
DanielSchwartzCS <98420686+DanielSchwartzCS@users.noreply.github.com>
2025-04-08 05:42:19 -0700

    Class Parser test coverage expansion

--- src/test/java/com/classhole/compiler/parser/ParserTest.java
+++ src/test/java/com/classhole/compiler/parser/ParserTest.java
@@ -249,4 +249,127 @@ public class ParserTest {
     Program program = parse(code);
     assertNotNull(program);
   }
+
+  @Test
+  public void testClassDef_NonIdentifierClassName() {
+    String code = "class 123 { init() {} } println(\"foo\");";
+    assertThrows(ParseException.class, () -> parse(code));
+  }
+
+  @Test
+  public void testClassDef_NonIdentifierSuperClassName() {
+    String code = "class MyClass extends 456 { init() {} } MyClass myClass;";
+    assertThrows(ParseException.class, () -> parse(code));
+  }
+
+  @Test
+  public void testClassDef_FieldDeclarations() throws ParseException {
+    String code = """
+    class TestFields {
+      int field1;
+      boolean field2;
+      void field3;
+      CustomType field4;
+      init() {}
+    }
+    TestFields t;
+    """;
+    Program program = parse(code);
+    assertNotNull(program);
+  }
+
+  @Test
+  public void testClassDef_InvalidFieldDeclaration() {
+    String code = "class TestClass { int 123; init() {} } TestClass t;";
+    assertThrows(ParseException.class, () -> parse(code));
+  }
+
+  @Test
+  public void testClassDef_MissingInit() {
+    String code = "class TestClass { notInit() {} } TestClass t;";
+    assertThrows(ParseException.class, () -> parse(code));
+  }
+
+  @Test
+  public void testMethodDef_NonIdentifierMethodName() {
+    String code = """
+    class TestClass {
+      init() {}
+      method 123() void {}
+    }
+    TestClass t;
+    """;
+    assertThrows(ParseException.class, () -> parse(code));
+  }
+
+  @Test
+  public void testMethodDef_ReturnTypes() throws ParseException {
+    String code = """
+    class TestClass {
+      init() {}
+      method method1() int { return 1; }
+      method method2() boolean { return true; }
+      method method3() void {}
+      method method4() CustomType { return new CustomType(); }
+    }
+    TestClass t;
+    """;
+    Program program = parse(code);
+    assertNotNull(program);
+  }
+
+  @Test
+  public void testMethodDef_InvalidReturnType() {
+    String code = """
+    class TestClass {
+      init() {}
+      method badMethod() 123 {}
+    }
+    TestClass t;
+    """;
+    assertThrows(ParseException.class, () -> parse(code));
+  }
+
+  @Test
+  public void testClassDef_WithSuperConstructorCall() throws ParseException {
+    String code = """
+    class Child extends Parent {
+      init() {
+        super(1, true, "test");
+      }
+    }
+    Child c;
+    """;
+    Program program = parse(code);
+    assertNotNull(program);
+  }
+
+  @Test
+  public void testClassDef_WithFieldsAndMethods() throws ParseException {
+    // Comprehensive test covering multiple aspects of class parsing
+    String code = """
+    class CompleteClass extends BaseClass {
+      int field1;
+      boolean field2;
+      CustomType field3;
+      
+      init(int param1, boolean param2) {
+        super(param1, param2);
+        field1 = param1;
+        field2 = param2;
+      }
+      
+      method calculate(int a, int b) int {
+        return a + b;
+      }
+      
+      method doSomething() void {
+        println("Doing something");
+      }
+    }
+    CompleteClass c;
+    """;
+    Program program = parse(code);
+    assertNotNull(program);
+  }
 }
